04ba90946789999a73ef93852c8c423a32b19eeb
rikaehler <richy.kaehler@gmail.com>
2026-02-25 19:49:28 +0100

    added verbose flag + check for extra info in output

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ client = genai.Client(api_key=api_key)
 # read user imput after  "uv run main.py "xxxxxxx" " and add into args.user_prompt 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 
 # create a new list of types.Content and set args.user_prompt as message 
@@ -26,7 +27,6 @@ messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)]
 
 def main():
     print("Hello from CLI aiagent written in python!")
-
 
     # generate content from model and put in into response
     response = client.models.generate_content(
@@ -39,13 +39,17 @@ def main():
             "API Request failed or returned no usage metadata. "
             "Check your connection or safety filter settings."
         )
-    x = response.usage_metadata.prompt_token_count
-    y = response.usage_metadata.candidates_token_count
+    
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
 
-    # print to CLI prompt, tokens and response
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {x}")
-    print(f"Response tokens: {y}")
+    # check if --verbose flag is included then prompt, tokens and response
+    if args.verbose: 
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+    
+    # always print response
     print(f" Response: {response.text}")
 
 if __name__ == "__main__":
