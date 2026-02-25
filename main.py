072308377c479678349334cb1e8b9b2972dd45e0
rikaehler <richy.kaehler@gmail.com>
2026-02-25 18:49:05 +0100

    added generate.contens and model

--- main.py
+++ main.py
@@ -5,10 +5,21 @@ from google import genai
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
+if api_key is None:
+    raise RuntimeError(
+        "MY_API_KEY not found. Please ensure it is defined in your .env file "
+            "and that the file is in the root directory."
+    )
+
+client = genai.Client(api_key=api_key)
 
 def main():
     print("Hello from aiagent!")
 
+    response = client.models.generate_content(
+    model='gemini-2.5-flash', contents='"Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."'
+    )
+    print(response.text)
 
 if __name__ == "__main__":
     main()
